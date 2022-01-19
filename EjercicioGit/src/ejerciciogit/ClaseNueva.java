fc72d53febb8f5ebb6ab8b196aa39a6337b4f8b2
Liam <Liam@DESKTOP-5VP63DL>
2022-01-19 09:52:25 +0100

    Lineas en el método main

---
+++ EjercicioGit/src/ejerciciogit/ClaseNueva.java
@@ -0,0 +1,12 @@
+package ejerciciogit;
+
+/**
+ *
+ * @author Liam
+ */
+public class ClaseNueva {
+
+    public static void main(String[] args) {
+        System.out.println("En la rama. Se crea la clase nueva");
+    }
+}
